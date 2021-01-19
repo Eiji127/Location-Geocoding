3f396305315130585cbcc177e855a554d8564b97
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-01-20 02:52:40 +0900

    [Upload]: add some files...

--- LocationNames/ViewController.swift
+++ LocationNames/ViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
+    
+    private let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(map)
+        title = "Home"
+        LocationManager.shared.getUserLocation { [weak self] location in //Weak selfはメモリリークを防ぐため
+            DispatchQueue.main.async {
+                guard let strongSelf = self else {
+                    return
+                }
+                strongSelf.addMapPin(with: location)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = view.bounds
+    }
+    
+    func addMapPin(with location: CLLocation) {
+        let pin = MKPointAnnotation()
+        pin.coordinate = location.coordinate
+        map.setRegion(MKCoordinateRegion(center: location.coordinate,
+                                         span: MKCoordinateSpan(
+                                            latitudeDelta: 0.7,
+                                            longitudeDelta: 0.7
+                                         )
+        ),
+                      animated: true)
+        map.addAnnotation(pin)
+        
+        LocationManager.shared.resolveLocationName(with: location) { [weak self] LocationName in
+            self?.title = LocationName
+        }
     }
 
 
