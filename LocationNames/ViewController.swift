4d6034b362c006ea339abda10765e2f841230882
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-01-20 01:55:42 +0900

    Initial Commit

---
+++ LocationNames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationNames
+//
+//  Created by 白数叡司 on 2021/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
